1d728c4645c15522ae5546c8c781ccaee9578ec6
rubymasciangelo <rubymasciangelo@gmail.com>
2020-08-27 15:54:22 -0700

    comments added to js

--- hhscript.js
+++ hhscript.js
@@ -1,9 +1,13 @@
+// execute bannerFunction on user scroll
 window.onscroll = function() {bannerFunction()};
 
+// target #banner
 var banner = document.getElementById("banner");
 
+// define offset position of banner
 var sticky = banner.offsetTop;
 
+// add sticky class to banner when you reach the scroll position and remove when you leave the scroll position
 function bannerFunction() {
     if (window.pageYOffset >= sticky) {
         banner.classList.add("sticky")
