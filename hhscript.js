e810ccf7600f2c12de4530e0d412dfbf64399368
rubymasciangelo <rubymasciangelo@gmail.com>
2020-08-27 22:22:18 -0700

    automatic banner msg changing added in js

--- hhscript.js
+++ hhscript.js
@@ -37,3 +37,19 @@ function showMsgs(n) {
     msgs[msgIndex-1].style.display = "block";
     dots[msgIndex-1].className += " active";
 }
+
+//auto
+var msgIndex = 0;
+showMsgs();
+
+function showMsgs() {
+    var i;
+    var msgs = document.getElementsByClassName("msg");
+    for (i=0; i < msgs.length; i++) {
+        msgs[i].style.display = "none";
+    }
+    msgIndex++;
+    if (msgIndex > msgs.length) {msgIndex = 1}
+    msgs[msgIndex-1].style.display = "block";
+    setTimeout(showMsgs, 15000);
+}
