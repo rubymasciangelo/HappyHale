1c337835850132438dd1572f4bd166bf269756a2
rubymasciangelo <rubymasciangelo@gmail.com>
2020-09-10 00:21:26 -0700

    css added for new crossfade slideshow plan

--- hhscript.js
+++ hhscript.js
@@ -2,18 +2,3 @@
 window.onload = function() {
     document.getElementById('navlogo').style.display = 'block';
 } ;
-
-var mbIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mbslide");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    mbIndex++;
-    if (mbIndex > x.length) {mbIndex = 1}
-    x[mbIndex-1].style.display = "block";
-    setTimeout(carousel, 5000);
-}
