2992c4e3475717d07fa4a1b3fe8f3e7d058c40c5
rubymasciangelo <rubymasciangelo@gmail.com>
2020-09-13 12:34:29 -0700

    js logo fix attempt

--- hhhomescript.js
+++ hhhomescript.js
@@ -13,3 +13,23 @@ function showMsgs() {
     msgs[msgIndex-1].style.display = "block";
     setTimeout(showMsgs, 3000);
 }
+
+// execute logoFunction on user scroll
+window.onscroll = function () { logoFunction(); };
+// calculate height
+var homeHeight = document.getElementById("home").clientHeight;
+// target homemsg
+var homeMsg = document.getElementById("homemsg");
+// target navlogo
+var navLogo = document.getElementById("navlogo");
+// switch out homeMsg for navLogo after homeHeight is surpassed
+function logoFunction() {
+    if (window.pageYOffset < homeHeight) {
+        navLogo.style.display = 'none';
+        homeMsg.style.display = 'block';
+    }
+    else {
+        navLogo.style.display = 'block';
+        homeMsg.style.display = 'none';
+    }
+}
--- homeHeight.js
+++ homeHeight.js
@@ -1,14 +1,14 @@
 // execute logoFunction on user scroll
 window.onscroll = function () { logoFunction(); };
 // calculate height
-var coverHeight = document.getElementById("cover").clientHeight;
+var homeHeight = document.getElementById("home").clientHeight;
 // target homemsg
 var homeMsg = document.getElementById("homemsg");
 // target navlogo
 var navLogo = document.getElementById("navlogo");
 // switch out homeMsg for navLogo after homeHeight is surpassed
 function logoFunction() {
-    if (window.pageYOffset < coverHeight) {
+    if (window.pageYOffset < homeHeight) {
         navLogo.style.display = 'none';
         homeMsg.style.display = 'block';
     }
