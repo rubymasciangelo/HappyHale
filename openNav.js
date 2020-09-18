6484b1dd3ca1b43f5227b9b6f8050039169dff98
rubymasciangelo <rubymasciangelo@gmail.com>
2020-09-17 23:41:58 -0700

    js hide/show smolnavbar based on scroll added

--- openNav.js
+++ openNav.js
@@ -1,9 +1,21 @@
-/*show navbar*/
+//show nav//
 function openNav() {
     document.getElementById("bbSmolnav").style.width = "250px";
 }
 
-/*hide navbar*/
+//hide nav//
 function closeNav() {
     document.getElementById("bbSmolnav").style.width = "0";
+}
+
+//hide navbar when scroll down, show navbar when scroll up//
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollpos = window.pageYOffset;
+    if (prevScrollpos > currentScrollpos) {
+        document.getElementById("smolnavbar").style.top = "0";
+    } else {
+        document.getElementById("smolnavbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollpos;
 }
