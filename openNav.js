26a504e571f8f0e10989f2b1a0384befbdb92c86
rubymasciangelo <rubymasciangelo@gmail.com>
2020-09-17 23:01:01 -0700

    js for collapse sidepanel created

---
+++ openNav.js
@@ -0,0 +1,9 @@
+/*show navbar*/
+function openNav() {
+    document.getElementById("bbSmolnav").style.width = "250px";
+}
+
+/*hide navbar*/
+function closeNav() {
+    document.getElementById("bbSmolnav").style.width = "0";
+}
